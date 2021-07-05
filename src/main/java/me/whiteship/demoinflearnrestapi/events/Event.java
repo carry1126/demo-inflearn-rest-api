e0c6211036d91351bf97c20577f1fdc8c72319b6
minjikim <carrykim1126@gmail.com>
2021-07-05 22:15:07 +0900

    Event domain

---
+++ src/main/java/me/whiteship/demoinflearnrestapi/events/Event.java
@@ -0,0 +1,26 @@
+package me.whiteship.demoinflearnrestapi.events;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Builder @AllArgsConstructor @NoArgsConstructor
+@Getter @Setter @EqualsAndHashCode(of = "id")
+public class Event {
+
+    private Integer id;
+    private String name;
+    private String description;
+    private LocalDateTime beginEnrollmentDateTime;    //이벤트등록일시
+    private LocalDateTime closeEnrollmentDateTime;    //이벤트등록종료일시
+    private LocalDateTime beginEventDateTime;         //이벤트시작일시
+    private LocalDateTime endEventDateTime;           //이벤트종료일시
+    private String location;                          // (optional) 이게 없으면 온라인 모임
+    private int basePrice;                            // (optional)
+    private int maxPrice;                             // (optional)
+    private int limitOfEnrollment;
+    private boolean offline;
+    private boolean free;
+    private EventStatus eventStatus;
+
+}
